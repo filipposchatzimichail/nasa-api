86e80d6ba175bed99c528170da2178a66e81390e
mask2011 <cmaskinis@hotmail.com>
2021-12-10 01:27:43 +0200

    Filter mars rover camera selection by rover selection

---
+++ Nasa.Apod.App/wwwroot/js/marsRoverPhoto.js
@@ -0,0 +1,39 @@
+﻿var camerasPerRover = {
+    "Curiosity": [
+        "FHAZ",
+        "RHAZ",
+        "MAST",
+        "CHEMCAM",
+        "MAHLI",
+        "MARDI",
+        "NAVCAM"
+    ],
+    "Spirit": [
+        "FHAZ",
+        "RHAZ",
+        "NAVCAM",
+        "PANCAM",
+        "MINITES"
+    ],
+    "Opportunity": [
+        "FHAZ",
+        "RHAZ",
+        "NAVCAM",
+        "PANCAM",
+        "MINITES"
+    ]
+}
+
+$("#Rover").change(function () {
+    let roverText = $("#Rover option:selected")[0].text;
+
+    let roverCameras = camerasPerRover[roverText];
+
+    $.each($("#Camera option"), function (i, option) {
+        if (!option.value) {
+            return;
+        }
+
+        option.hidden = roverCameras.indexOf(option.text) < 0;
+    });
+})
